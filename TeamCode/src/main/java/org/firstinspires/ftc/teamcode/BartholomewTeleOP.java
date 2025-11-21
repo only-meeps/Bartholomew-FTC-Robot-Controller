16c8d83b7264a849ecbd96fd5c7ed8f24da507ff
AwesomeMeeps <ffkoenig@gmail.com>
2025-11-21 08:25:59 -0800

    Idk what I did

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BartholomewTeleOP.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BartholomewTeleOP.java
@@ -22,12 +22,15 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 
 public class BartholomewTeleOP extends LinearOpMode
 {
+    boolean dead = false;
+    boolean actuallyKilledItself = false;
     DcMotor frontLeftMotor;
     DcMotor frontRightMotor;
     DcMotor rearLeftMotor;
     DcMotor rearRightMotor;
     DcMotor weedWacker;
-    double weedWackerSpeed = 0.0;
+    DcMotor killer;
+    double weedWackerSpeed = 1.0;
     final double SPEED_INCREMENT = 0.01;  // Adjust this value to change the smoothness
     final double MAX_SPEED = 1.0;
     final double MIN_SPEED = 0.0;
@@ -41,107 +44,124 @@ public class BartholomewTeleOP extends LinearOpMode
         rearRightMotor = hardwareMap.get(DcMotor.class, "rr_motor");
         weedWacker = hardwareMap.get(DcMotor.class, "weedwacker");
 
-        waitForStart();
-
-        while (opModeIsActive())
+        if(actuallyKilledItself)
         {
-            double leftStickY = gamepad1.left_stick_y;
-            double leftStickX = -gamepad1.left_stick_x;
-            double rightStickX = -gamepad1.right_stick_x;
-            double rightStickY = gamepad1.right_stick_y;
-
-            double leftPower = leftStickY + leftStickX * 0.5;
-            double rightPower = -leftStickY + leftStickX * 0.5;
-
-            if (leftStickX > 0)
-            {
-                rearLeftMotor.setPower(leftPower);
-                frontLeftMotor.setPower(leftPower);
-                frontRightMotor.setPower(rightPower);
-                rearRightMotor.setPower(-rightPower);
-            }
-            else if (leftStickX < 0)
-            {
-                rearLeftMotor.setPower(leftPower);
-                frontLeftMotor.setPower(leftPower);
-                rearRightMotor.setPower(-rightPower);
-                frontRightMotor.setPower(rightPower);
-            }
-            else
-            {
-                rearLeftMotor.setPower(leftPower);
-                frontLeftMotor.setPower(leftPower);
-                rearRightMotor.setPower(-rightPower);
-                frontRightMotor.setPower(rightPower);
-            }
-
-            if (gamepad1.right_bumper)
-            {
-                weedWacker.setPower(weedWackerSpeed);
-            }
-            else if (gamepad1.left_bumper)
-            {
-                weedWacker.setPower(-weedWackerSpeed);
-            }
-            else
-            {
-                weedWacker.setPower(0.0);
-            }
-
-            //Smooth speed changes
-            if (gamepad1.dpad_up && weedWackerSpeed < MAX_SPEED)
-            {
-                weedWackerSpeed += SPEED_INCREMENT;
-                weedWackerSpeed = Math.min(weedWackerSpeed, MAX_SPEED); //Ensure it doesn't exceed max
-            }
-            else if (gamepad1.dpad_down && weedWackerSpeed > MIN_SPEED)
-            {
-                weedWackerSpeed -= SPEED_INCREMENT;
-                weedWackerSpeed = Math.max(weedWackerSpeed, MIN_SPEED); //Ensure it doesn't go below min
-            }
-
-            if (gamepad1.right_trigger > 0.3)
-            {
-                rearRightMotor.setPower(1.0);
-                frontRightMotor.setPower(1.0);
-            }
-            if (gamepad1.left_trigger > 0.3)
-            {
-                rearLeftMotor.setPower(-1.0);
-                frontLeftMotor.setPower(-1.0);
-            }
-
-            //This is causing issues.
-            if (gamepad1.triangle)
-            {
-                rearLeftMotor.setPower(1);
-                frontLeftMotor.setPower(1);
-                rearRightMotor.setPower(-1);
-                frontRightMotor.setPower(-1);
-            }
-
-            if (gamepad1.dpad_right && 0.5 < 1)
-            {
-                double speed = 0.5;
-                speed += SPEED_INCREMENT;
-                speed = Math.min(speed, 1.0);
-                leftPower = leftStickY + leftStickX * speed;
-                rightPower = -leftStickY + leftStickX * speed;
-
-            }
-            if (gamepad1.dpad_left && 0.5 > 0)
-            {
-                double speed = 0.5;
-                speed -= SPEED_INCREMENT;
-                speed = Math.max(speed, 0.0);
-                leftPower = leftStickY + leftStickX * speed;
-                rightPower = -leftStickY + leftStickX * speed;
-
-            }
+            killer = hardwareMap.get(DcMotor.class, "deader");
         }
-        rearLeftMotor.setPower(0.0);
-        frontLeftMotor.setPower(0.0);
-        rearRightMotor.setPower(0.0);
-        frontRightMotor.setPower(0.0);
+        waitForStart();
+        if(!dead)
+        {
+            while (opModeIsActive())
+            {
+                double leftStickY = gamepad1.left_stick_y;
+                double leftStickX = gamepad1.left_stick_x;
+                double rightStickX = -gamepad1.right_stick_x;
+                double rightStickY = gamepad1.right_stick_y;
+
+                double leftPower = leftStickY + leftStickX;
+                double rightPower = -leftStickY + leftStickX;
+                if(actuallyKilledItself)
+                {
+                    killer = hardwareMap.get(DcMotor.class, "you have been fucked haha");
+                }
+                if(gamepad1.left_stick_button)
+                {
+                    actuallyKilledItself = true;
+                }
+                if (leftStickX != 0)
+                {
+                    rearLeftMotor.setPower(leftStickX);
+                    frontLeftMotor.setPower(leftStickX);
+                    frontRightMotor.setPower(leftStickX);
+                    rearRightMotor.setPower(leftStickX);
+                }
+                else if(rightStickY != 0)
+                {
+                    rearLeftMotor.setPower(rightStickY);
+                    frontLeftMotor.setPower(rightStickY);
+                    frontRightMotor.setPower(-rightStickY);
+                    rearRightMotor.setPower(-rightStickY);
+                }
+                else
+                {
+                    rearLeftMotor.setPower(0);
+                    frontLeftMotor.setPower(0);
+                    frontRightMotor.setPower(0);
+                    rearRightMotor.setPower(0);
+                }
+                if (gamepad1.right_bumper)
+                {
+                    weedWacker.setPower(weedWackerSpeed);
+                }
+                else if (gamepad1.left_bumper)
+                {
+                    weedWacker.setPower(-weedWackerSpeed);
+                }
+                else
+                {
+                    weedWacker.setPower(0.0);
+                }
+
+                //Smooth speed changes
+                if (gamepad1.dpad_up && weedWackerSpeed < MAX_SPEED)
+                {
+                    weedWackerSpeed += SPEED_INCREMENT;
+                    weedWackerSpeed = Math.min(weedWackerSpeed, MAX_SPEED); //Ensure it doesn't exceed max
+                }
+                else if (gamepad1.dpad_down && weedWackerSpeed > MIN_SPEED)
+                {
+                    weedWackerSpeed -= SPEED_INCREMENT;
+                    weedWackerSpeed = Math.max(weedWackerSpeed, MIN_SPEED); //Ensure it doesn't go below min
+                }
+
+                if (gamepad1.right_trigger > 0.3)
+                {
+                    rearLeftMotor.setPower(gamepad1.right_trigger);
+                    frontLeftMotor.setPower(-gamepad1.right_trigger);
+                    frontRightMotor.setPower(-gamepad1.right_trigger);
+                    rearRightMotor.setPower(gamepad1.right_trigger);
+                }
+                if (gamepad1.left_trigger > 0.3)
+                {
+                    rearLeftMotor.setPower(-gamepad1.left_trigger);
+                    frontLeftMotor.setPower(gamepad1.left_trigger);
+                    frontRightMotor.setPower(gamepad1.left_trigger);
+                    rearRightMotor.setPower(-gamepad1.left_trigger);
+                }
+
+                //This is causing issues.
+                if (gamepad1.triangle)
+                {
+                    rearLeftMotor.setPower(1);
+                    frontLeftMotor.setPower(1);
+                    rearRightMotor.setPower(-1);
+                    frontRightMotor.setPower(-1);
+                }
+
+                if (gamepad1.dpad_right)
+                {
+                    double speed = 0.5;
+                    speed += SPEED_INCREMENT;
+                    speed = Math.min(speed, 1.0);
+                    leftPower = leftStickY + leftStickX * speed;
+                    rightPower = -leftStickY + leftStickX * speed;
+
+                }
+                if (gamepad1.dpad_left)
+                {
+                    double speed = 0.5;
+                    speed -= SPEED_INCREMENT;
+                    speed = Math.max(speed, 0.0);
+                    leftPower = leftStickY + leftStickX * speed;
+                    rightPower = -leftStickY + leftStickX * speed;
+
+                }
+            }
+            rearLeftMotor.setPower(0.0);
+            frontLeftMotor.setPower(0.0);
+            rearRightMotor.setPower(0.0);
+            frontRightMotor.setPower(0.0);
+        }
     }
+
 }
