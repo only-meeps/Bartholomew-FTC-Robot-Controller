5ec81caa7201470f9ad66a4c90cd3f10c9f4b76b
hi <30905705+1bit0@users.noreply.github.com>
2026-01-21 09:49:09 -0800

    better clock shit
    dsigubaspvbunaspiubin8egha;nvobilDS HAgbti5h blKVDN

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MotorClock.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MotorClock.java
@@ -22,14 +22,14 @@ public class MotorClock extends LinearOpMode {
         clockMotor = hardwareMap.get(DcMotorEx.class, "clockMotor");
 
         waitForStart();
-
-        Calendar calendar = Calendar.getInstance(TimeZone.getTimeZone("America/Los_Angeles"));
-        int secondsWithinMinute = calendar.get(Calendar.SECOND);
+        
         clockMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        clockMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-
+        clockMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        clockMotor.setPower(1);
         int secondStep = ticksPerRevolution / 60;
         while (opModeIsActive()) {
+            Calendar calendar = Calendar.getInstance(TimeZone.getTimeZone("America/Los_Angeles"));
+            int secondsWithinMinute = calendar.get(Calendar.SECOND);
             clockMotor.setTargetPosition(secondStep * secondsWithinMinute);
         }
     }
