34cbc721f1613488761cf581fc409588b307f60a
Bastian <bwildetalk@gmail.com>
2026-02-24 17:18:42 -0800

    Merge branch 'main' of https://github.com/only-meeps/Bartholomew-FTC-Robot-Controller
    # Conflicts:
    #	TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BartholomewAutonomousMk2.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Wheelie.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Wheelie.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.IMU;
 
 import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
@@ -11,20 +12,38 @@ import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
 @Autonomous
 public class Wheelie extends LinearOpMode {
     @Override
-    public void runOpMode(){
+    public void runOpMode() throws InterruptedException {
         IMU imu = hardwareMap.get(IMU.class, "imu");
+        DcMotor rearLeftMotor = hardwareMap.get(DcMotor.class, "rl_motor");
+        DcMotor rearRightMotor = hardwareMap.get(DcMotor.class, "rr_motor");
         IMU.Parameters parameters = new IMU.Parameters(
                 new RevHubOrientationOnRobot(RevHubOrientationOnRobot.LogoFacingDirection.UP,
                         RevHubOrientationOnRobot.UsbFacingDirection.BACKWARD));
         imu.initialize(parameters);
+        waitForStart();
+        wait(1000);
+        telemetry.addLine("Please hold robot upright on the back wheels until this message disappears.");
+        telemetry.update();
+        wait(10000);
+        telemetry.update();
+
+
 
         while (opModeIsActive()) {
             YawPitchRollAngles yawPitchRollAngles = imu.getRobotYawPitchRollAngles();
-            telemetry.addLine("IMU initialized.");
             telemetry.addData("Yaw", yawPitchRollAngles.getYaw());
             telemetry.addData("Pitch", yawPitchRollAngles.getPitch());
             telemetry.addData("Roll", yawPitchRollAngles.getRoll());
             telemetry.update();
+
+            if (yawPitchRollAngles.getPitch() < -90){
+                rearLeftMotor.setPower(1);
+                rearRightMotor.setPower(1);
+            }
+            else if (yawPitchRollAngles.getPitch() > -90){
+                rearLeftMotor.setPower(-1);
+                rearRightMotor.setPower(-1);
+            }
         }
     }
 }
